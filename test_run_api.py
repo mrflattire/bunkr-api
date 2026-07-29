6643731527de630c6ac4beacb58f969194cb5844
mrflattire <yourwebsiteasked@gmail.com>
2026-07-29 13:26:36 +0300

    Test run the api facade

--- test_run_api.py
+++ test_run_api.py
@@ -6,13 +6,13 @@ api = BunkrAPI()
 
 # 1. Search and Resolve
 async def main():
-    results = await api.search("Trippie Red")
+    results = await api.search("Natalie Roush")
     if results:
         # Resolve the first result into our database
         album_id = await api.resolve_album(results[0]['url'])
         print(f"Registered album with ID: {album_id}")
         
         # 2. Download it
-        api.download_album(album_id, workers=5)
+        await api.download_album(album_id, workers=5)
 
 asyncio.run(main())
